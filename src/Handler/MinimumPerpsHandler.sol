0c827a898d7be8344e47dc4389d7e214dd5bf38a
MrSaade <saadesamir03@gmail.com>
2025-02-13 13:44:54 +0000

    init

---
+++ src/Handler/MinimumPerpsHandler.sol
@@ -0,0 +1,71 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import {MinimumPerps} from "../MinimumPerpsFuzzed.sol";
+import {IERC20} from "openzeppelin-contracts/contracts/interfaces/IERC20.sol";
+
+import {MockERC20} from "../../test/Mock/MockERC20.sol";
+import {MockAggregatorV3} from "../../test/Mock/MockAggregatorV3.sol";
+
+import "forge-std/Test.sol";
+
+contract MinimumPerpsHandler is Test {
+    MinimumPerps public minimumPerps;
+    MockERC20 public usdc;
+    MockAggregatorV3 public pricefeed;
+
+    constructor(
+        MinimumPerps _minimumPerps,
+        MockERC20 _usdc,
+        MockAggregatorV3 _pricefeed
+    ) {
+        minimumPerps = _minimumPerps;
+        usdc = _usdc;
+        pricefeed = _pricefeed;
+    }
+
+    function deposit(uint256 assets, address receiver) external {
+        minimumPerps.deposit(assets, receiver);
+    }
+
+    function withdraw(uint256 amount) external {
+        minimumPerps.withdraw(amount, msg.sender, msg.sender);
+    }
+
+    function increasePosition(
+        bool isLong,
+        uint256 sizeDeltaUsd,
+        uint256 collateralDelta
+    ) external {
+        minimumPerps.increasePosition(
+            isLong,
+            sizeDeltaUsd,
+            collateralDelta,
+            msg.sender
+        );
+    }
+
+    function decreasePosition(
+        bool isLong,
+        uint256 sizeDeltaUsd,
+        uint256 collateralDelta
+    ) external {
+        minimumPerps.decreasePosition(
+            isLong,
+            sizeDeltaUsd,
+            collateralDelta,
+            msg.sender
+        );
+    }
+
+    function liquidate(bool isLong) external {
+        minimumPerps.liquidate(msg.sender, isLong);
+    }
+
+    function setPrice(uint256 seed) external {
+        // BTC price in the range $45000-$55000
+        uint256 newPrice = bound(seed, 45_000e8, 55_000e8);
+
+        pricefeed.setPrice(int256(newPrice));
+    }
+}
---
+++ test/FuzzMinimumPerps.t.sol
@@ -0,0 +1,134 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "../src/MinimumPerpsFuzzed.sol";
+import {IERC20} from "openzeppelin-contracts/contracts/interfaces/IERC20.sol";
+import {MockERC20} from "./Mock/MockERC20.sol";
+import {MockAggregatorV3} from "./Mock/MockAggregatorV3.sol";
+import {IAggregatorV3} from "../src/Interfaces/IAggregatorV3.sol";
+import {Errors} from "../src/Errors.sol";
+import {IOracle, Oracle} from "../src/Oracle.sol";
+import {StdInvariant} from "forge-std/StdInvariant.sol";
+import {MinimumPerpsHandler} from "../src/Handler/MinimumPerpsHandler.sol";
+
+contract MinimumPerpsTest is StdInvariant, Test {
+    MinimumPerps public minimumPerps;
+    MinimumPerpsHandler public minimumPerpsHandler;
+
+    error TooMuchDeposits();
+
+    address public alice = address(1);
+    address public bob = address(2);
+
+    MockERC20 public USDC;
+    MockERC20 public BTC;
+    string public constant name = "MinPerps";
+    string public constant symbol = "MP";
+    MockAggregatorV3 public btcFeed;
+    MockAggregatorV3 public usdcFeed;
+    uint8 public constant feedDecimals = 8;
+    uint256 public constant heartbeat = 3600;
+
+    // (50_000 * 1e30) / (50_000 * 1e8 * priceFeedFactor) = 1e8
+    // E.g. $50,000 converts to 1 Bitcoin (8 decimals) when the price is $50,000 per BTC
+    // => priceFeedFactor = 1e14
+    uint256 public constant btcPriceFeedFactor = 1e14;
+
+    uint256 public constant usdcPriceFeedFactor = 1e16;
+
+    function setUp() public {
+        USDC = new MockERC20("USDC", "USDC", 6);
+        BTC = new MockERC20("BTC", "BTC", 8);
+
+        btcFeed = new MockAggregatorV3(
+            feedDecimals, //decimals
+            "BTC", //description
+            1, //version
+            0, //roundId
+            int256(50_000 * 10 ** feedDecimals), //answer
+            0, //startedAt
+            0, //updatedAt
+            0 //answeredInRound
+        );
+
+        usdcFeed = new MockAggregatorV3(
+            feedDecimals, //decimals
+            "USDC", //description
+            1, //version
+            0, //roundId
+            int256(1 * 10 ** feedDecimals), //answer
+            0, //startedAt
+            0, //updatedAt
+            0 //answeredInRound
+        );
+
+        IOracle oracleContract = new Oracle();
+
+        oracleContract.updatePricefeedConfig(
+            address(USDC),
+            IAggregatorV3(usdcFeed),
+            heartbeat,
+            usdcPriceFeedFactor
+        );
+
+        oracleContract.updatePricefeedConfig(
+            address(BTC),
+            IAggregatorV3(btcFeed),
+            heartbeat,
+            btcPriceFeedFactor
+        );
+
+        minimumPerps = new MinimumPerps(
+            name,
+            symbol,
+            address(BTC),
+            IERC20(USDC),
+            IOracle(oracleContract),
+            0 // Borrowing fees deactivated by default
+        );
+
+        minimumPerpsHandler = new MinimumPerpsHandler(
+            minimumPerps,
+            USDC,
+            btcFeed
+        );
+        targetContract(address(minimumPerpsHandler));
+
+        targetSender(bob);
+        vm.prank(bob);
+        minimumPerps.approve(address(minimumPerpsHandler), type(uint256).max);
+
+        targetSender(alice);
+        vm.prank(alice);
+        minimumPerps.approve(address(minimumPerpsHandler), type(uint256).max);
+
+        uint256 usdcLiquidityDeposit = 1_000_000e6;
+
+        deal(address(USDC), bob, usdcLiquidityDeposit);
+        vm.startPrank(bob);
+        USDC.approve(address(minimumPerps), usdcLiquidityDeposit);
+        minimumPerps.deposit(usdcLiquidityDeposit, bob);
+
+        vm.stopPrank();
+
+        deal(address(USDC), alice, usdcLiquidityDeposit);
+        vm.startPrank(alice);
+        USDC.approve(address(minimumPerps), usdcLiquidityDeposit);
+        minimumPerps.deposit(usdcLiquidityDeposit, alice);
+
+        vm.stopPrank();
+    }
+
+    function invariant_OINotOverutilized() public {
+        uint256 depositedLiquidity = minimumPerps.totalDeposits();
+
+        uint256 netOI = minimumPerps.openInterestLong() +
+            minimumPerps.openInterestShort();
+
+        uint256 collateralPrice = minimumPerps.getCollateralPrice();
+
+        // Open interest should not be more than 50% of the available liquidity
+        assert(netOI <= (depositedLiquidity * collateralPrice) / 2);
+    }
+}
